a961175b17122c67776af0e810fb0c8a7b2462da
ajeet-05 <ajeetkuntal0005@gmail.com>
2026-04-11 13:57:56 +0530

    JavaScript Day 03

---
+++ JavaScript/Day 02/script.js
@@ -0,0 +1,7 @@
+// What i learn today:-
+// 01). Introduction to JS? 
+// 02). why it is important?
+// 03). what can it do for you?
+// 04). script tag use karke attach kar na h JS.
+// 05). JS ko browser me run karna.
+// 06). what is scripting language, programming language, styling language and markup language?
---
+++ JavaScript/Day 03/script.js
@@ -0,0 +1,80 @@
+// What i learn today:-
+// 01). var, let and const?
+// 02). console.... promot alert?
+// 03). string kya hoti h?
+// 04). slice, template string(template literals), split, replace and includes?
+// 05). statement expression?
+
+
+// 01). var, let and const :-
+// var :-
+// var a;// decalaration
+// a = 5; // initialization
+// var a = 5; // decalaration and initialization
+
+// a = 10; // reinitialization
+
+// var a = 10; // redecalaration
+
+// let :-
+
+// let b; // decalaration
+
+// b = 5; // initialization
+
+// b = 10; // reinitialization
+
+// redecalaration not allowed in let.
+
+
+// const :-
+
+// c = 5; // decalaration
+
+// c = 5; // initialization
+
+// const c = 5; // decalaration and initialization
+
+// redecalaration and reinitialization is not allowed in const.
+
+
+
+// 02). console.... prompt alert :-
+
+// prompt alert :-
+
+// let name = prompt ("Enter your nm");
+
+// console :-
+
+// console.log(name);
+// console.error(name);
+// console.warn(name);
+// console.info(name);
+
+// console.table() :-
+
+// let user = {
+//     name: "Sheriyans",
+//     email: "hello@gmail.com",
+//     course: "JS"
+// };
+
+// console.table(user);
+
+// console.group() and console.groupEnd() :-
+
+// console.group("User Info");
+// console.log("Name : Ajeet");
+// console.log("Course : Cohort 2.0");
+// console.groupEnd();
+
+// console.time() and console.timeEnd() :-
+
+// console.time("loopTime");
+// for(let i = 0; i < 1000; i++) {}
+// console.timeEnd("loopTime");
+
+// console.clear() :-
+
+// console.clear();
