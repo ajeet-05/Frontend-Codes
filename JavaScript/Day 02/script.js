09607a71fca354c18329f5d69d93def150fb1d99
ajeet-05 <ajeetkuntal0005@gmail.com>
2026-04-11 14:12:24 +0530

    JavaScript Day 02

--- JavaScript/Day 02/script.js
+++ JavaScript/Day 02/script.js
@@ -6,3 +6,54 @@
 // 05). JS ko browser me run karna.
 // 06). what is scripting language, programming language, styling language and markup language?
 
+// 01). Introduction to JS :-
+// Ans:- JavaScript ek programming language hai
+// Ye mainly web pages ko interactive banane ke liye use hoti hai
+// HTML = structure
+// CSS = design
+// JS = behavior (action)
+
+// 👉 Example:
+
+// Button click → kuch ho
+// Form submit → data check ho
+// Page reload bina data update ho
+
+// 02). why it is important :-
+// Ans:- Har browser me support hoti hai
+// Frontend ka core part hai
+// Full-stack me bhi use hoti hai (Node.js)
+// Real-time apps bana sakte ho (chat apps, games)
+
+// 👉 Simple: Without JS → website boring 😴
+
+// 03). what can it do for you :-
+// Ans:- Button click handle karna
+// Form validation
+// Animations
+// API se data lana
+// Games banana
+// Mobile apps (React Native)
+// Backend (Node.js)
+
+// 04). script tag use karke attach kar na h JS :-
+// Ans:- <script src="script.js"></script>
+// <script>
+//   console.log("Hello JS");
+// </script>
+
+// 05). JS ko browser me run karna :-
+// Ans:- 3 simple ways:
+
+// Browser console me (Inspect → Console)
+// HTML file me <script> tag se
+// External .js file se
+
+
+// 06). what is scripting language, programming language, styling language and markup language :-
+// Ans:- | Type        | Kaam                       | Example      |
+// | ----------- | -------------------------- | ------------ |
+// | Programming | Logic + apps banana        | Java, Python |
+// | Scripting   | Automation + browser tasks | JavaScript   |
+// | Styling     | Design                     | CSS          |
+// | Markup      | Structure                  | HTML         |
