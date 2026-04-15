c8e6e8a2e7eb562034137d8b0f93108274832810
ajeet-05 <ajeetkuntal0005@gmail.com>
2026-04-16 03:13:18 +0530

    JavaScript Day 03 adding Questions folder

---
+++ JavaScript/Day 03/Questions/script.js
@@ -0,0 +1,51 @@
+// 1. Create two variables for your full name and favorite hobby. Print: "My name is <name> and my hobby is <hobby>"
+let fullName = "Ajeet Singh Kuntal";
+let hobby = "Coding and Listing Music";
+console.log(fullName, hobby); // Ajeet Singh Kuntal Coding and Listing Music
+
+// 2. Perform the calculation 45 * 2 - 10 and print the result.
+var cal = 45*2-10;
+console.log(cal); // 80
+
+// 3. Print the current year using JavaScript's Date object.
+const d = new Date();
+let year = d.getFullYear();
+console.log(year); // 2026
+
+// 4. Store first name and last name separately and print the fullname.
+let firstName = "Ajeet";
+let lastName = "Singh Kuntal";
+console.log(firstName, lastName); // Ajeet Singh Kuntal
+
+// 5. Declare a variable, print it, update it, and print again.
+var a = 10;
+console.log(a); // 10
+
+a = 20;
+console.log(a); // 20
+
+// 6. Print a custom error message using console.error().
+console.error("This is a custome error message!");
+
+// 7. Store a number and print its square.
+let number = 5;
+let square = number * number;
+console.log(square); // 25
+
+// 8. Create a boolean variable and print its value.
+let isLearing = true;
+console.log(isLearing); // true
+
+// 9. Store your age and print whether it is greater than 18.
+let age = 20;
+if (age > 18) {
+    console.log("True: The age is greater than 18."); // True: The age is greater than 18.
+} else {
+    console.log("False: The age is greater than 18.");
+}
+
+// 10. Divide 100 by 0 and print the result.
+let num1 = 100;
+let num2 = 0;
+let divide = num1/num2;
+console.log(divide); // Infinity
