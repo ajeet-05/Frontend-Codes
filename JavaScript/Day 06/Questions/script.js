c62ec4fb6f2e8d4b44d85cffcd9e265016d5e794
ajeet-05 <ajeetkuntal0005@gmail.com>
2026-04-20 11:30:38 +0530

    JavaScript Day 06 Adding Questions folder

---
+++ JavaScript/Day 06/Questions/script.js
@@ -0,0 +1,210 @@
+// 1. What will be the output?
+// let a = 10 + 5 * 2;
+// console.log(a);
+
+let a = 10 + 5 * 2;
+console.log(a); // 20
+
+// 2. Predict output:
+// let x = 10;
+// x += 5;
+// console.log(x);
+
+let x = 10;
+x += 5;
+console.log(x); // 15
+
+// 3. What will be the result?
+// console.log(10 % 3);
+
+console.log(10 % 3); // 1
+
+// 4. Output?
+// let a = "Hello" + " " + "World";
+// console.log(a);
+
+let b = "Hello" + " " + "World";
+console.log(b); // Hello World
+
+// 5. What will be printed?
+// console.log(5 > 3);
+// console.log(5 < 3);
+
+console.log(5 > 3); // true
+console.log(5 < 3); // false
+
+// 6. Output:
+// console.log(5 == "5");
+// console.log(5 === "5");
+
+console.log(5 == "5"); // true
+console.log(5 === "5"); // false
+
+// 7. Predict:
+// console.log(true && false);
+// console.log(true || false);
+
+console.log(true && false); // false
+console.log(true || false); // true
+
+// 8. Output:
+// let a = 10;
+// console.log(a++);
+// console.log(a);
+
+let c = 10;
+console.log(c++); // 10
+console.log(c); // 11
+
+// 9. Output:
+// let a = 10;
+// console.log(++a);
+
+let d = 10;
+console.log(++d); // 11
+
+// 10. What will this return?
+// console.log(typeof 42);
+// console.log(typeof "JS");
+
+console.log(typeof 42); // number
+console.log(typeof "JS"); // String
+
+// 11. Predict output:
+// console.log("5" + 2);
+// console.log("5" - 2);
+
+console.log("5" + 2); // 52
+console.log("5" - 2); // 3
+
+// 12. Output:
+// console.log(0 == false);
+// console.log(0 === false);
+
+console.log(0 == false); // true
+console.log(0 === false); // false
+
+// 13. Predict:
+// console.log(null == undefined);
+// console.log(null === undefined);
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
+//. 14. Output:
+// console.log(!true);
+// console.log(!0);
+
+console.log(!true); // false
+console.log(!0); // true
+
+// 15. What will be the result?
+// let e = 5;
+// let f = 10;
+// console.log(e > 3 && f < 20);
+
+let e = 5;
+let f = 10;
+console.log(e > 3 && f < 20); // true
+
+// 16. Output:
+// console.log(5 & 1);
+// console.log(5 | 1);
+
+console.log(5 & 1); // 1
+console.log(5 | 1); // 5
+
+// 17. Predict:
+// console.log(2 ** 3);
+
+console.log(2 ** 3); // 8
+
+// 18. Output:
+// let g = "10";
+// let h = 10;
+// console.log(g == h && g === h);
+
+let g = "10";
+let h = 10;
+console.log(g == h && g === h); // false
+
+// 19. Output:
+// let i = 0;
+// console.log(i || "Default");
+
+let i = 0;
+console.log(i || "Default"); // Default
+
+// 20. Predict:
+// let j = null;
+// console.log(j ?? "Fallback");
+
+let j = null;
+console.log(j ?? "Fallback"); // Fallback
+
+// 21. Output:
+// console.log([] == false);
+
+console.log([] == false); // true
+
+// 22. Predict:
+// console.log([] == ![]);
+
+console.log([] == ![]); // true
+
+// 23. Output:
+// console.log(NaN == NaN);
+// console.log(NaN === NaN);
+
+console.log(NaN == NaN); // false
+console.log(NaN === NaN); // false
+
+// 24. What will be printed?
+// console.log(typeof NaN);
+
+console.log(typeof NaN); // number
+
+// 25. Predict:
+// let k = 10;
+// let l = "10";
+// console.log(k + l - k);
+
+let k = 10;
+let l = "10";
+console.log(k + l - k); // 100
+
+// 26. Output:
+// console.log("5" * "2");
+// console.log("5" / "2");
+
+// 27. Predict:
+// console.log(true + true);
+// console.log(true + false);
+
+console.log(true + true); // 2
+console.log(true + false); // 1
+
+// 28. Output:
+// let a = 1;
+// let b = a++;
+// let c = ++a;
+// console.log(a, b, c);
+
+let m = 1;
+let n = m++;
+let o = ++m;
+console.log(m, n, o); // 3, 1, 3
+
+// 29. Predict:
+// console.log(false || null || "Hello");
+// console.log(false && "Hello");
+
+console.log(false || null || "Hello"); // Hello
+console.log(false && "Hello"); // false
+
+// 30. Output:
+// console.log(0 ?? false);
+// console.log(0 || false);
+
+console.log(0 ?? false); // 0
+console.log(0 || false); // false
