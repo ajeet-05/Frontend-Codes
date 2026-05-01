5dbf8451dcc9981497f18e34550d62403551ab7e
ajeet-05 <ajeetkuntal0005@gmail.com>
2026-05-01 14:01:25 +0530

    JavaScript day 14 to 16 Adding Questions of Function and Array and Objects and String

--- JavaScript/Day 14 to 16/Questions of Function and Array and Objects and String/script2.js
+++ JavaScript/Day 14 to 16/Questions of Function and Array and Objects and String/script2.js
@@ -1,12 +1,52 @@
-// Arrays :-
+// // Arrays :-
 
-// 1. Create an array of movies and print using join(" - ").
-// 2. Print the element at index 1 of an array.
-// 3. Add two elements at the beginning using unshift().
-// 4. Remove the last element using pop().
-// 5. Extract the first three elements using slice().
-// 6. Find the index of number 5 using indexOf().
-// 7. Check if 3 exists using includes().
-// 8. Merge two arrays using concat().
-// 9. Sort an array of numbers in ascending order.
-// 10. Copy an array using spread operator and compare references.
+// // 1. Create an array of movies and print using join(" - ").
+// let arr = ["Kd:The Devil", "KGf1", "Toxic", "KGF2"];
+// console.log(arr.join(" - "));
+
+
+// // 2. Print the element at index 1 of an array.
+// console.log(arr[1]);
+
+
+// // 3. Add two elements at the beginning using unshift().
+// console.log(arr.unshift("Bhaubhali", "Yevdu3"));
+
+
+// // 4. Remove the last element using pop().
+// console.log(arr.pop());
+
+// // 5. Extract the first three elements using slice().
+// let threeelement = console.log(arr.slice(0, 3));
+
+
+// // 6. Find the index of number 5 using indexOf().
+// console.log(arr.indexOf(5));
+
+
+// // 7. Check if 3 exists using includes().
+// console.log(arr.includes(3));
+
+
+// // 8. Merge two arrays using concat().
+// let arr1 = [1, 2, 3, 4, 5];
+// console.log(arr1.concat(arr));
+
+
+// // 9. Sort an array of numbers in ascending order.
+// // arr sort raha h jo ki string ke form me h jo ki shedha shedah ho jata h:-
+// console.log(arr.sort());
+// // arr1 sort ho rha h jo ki function ke throw ho raha h:-
+// function sort1(a, b) {
+//     return a - b;
+// }
+// console.log(arr1.sort(sort1));
+
+
+// // 10. Copy an array using spread operator and compare references.
+// let copyArr1 = [...arr1];
+// console.log(arr1);
+// console.log(copyArr1);
+
+// console.log(arr1 === copyArr1);
+
--- JavaScript/Day 14 to 16/Questions of Function and Array and Objects and String/script4.js
+++ JavaScript/Day 14 to 16/Questions of Function and Array and Objects and String/script4.js
@@ -1,12 +1,51 @@
 // String :-
 
 // 1. Print the length of a string.
+let str = "Ajeet Singh Kuntal";
+console.log(str.length);
+
+
 // 2. Print last 4 characters using slice().
+let str1 = str.slice(14, 18);
+console.log(str1);
+
+
 // 3. Convert a string to lowercase.
+console.log(str.toLowerCase());
+
+
 // 4. Split a string into an array of words.
+console.log(str.split(" "));
+
+
 // 5. Find index of a character using indexOf().
+console.log(str.indexOf("Kuntal"));
+
+
 // 6. Replace a word in a string.
+console.log(str.replace("Ajeet", "Ajay"));
+
+
 // 7. Repeat a string three times.
+console.log(str.repeat(3));
+
+
 // 8. Check if a word exists in a string.
+console.log(str.includes("Jaat"));
+
+
 // 9. Remove all spaces from a string.
+console.log(str.split(" ").join(""));
+
+
 // 10. Count the number of vowels in a string.
+let count = 0;
+
+for (let char of str.toLowerCase()) {
+  if ("aeiou".includes(char)) {
+    count++;
+  }
+}
+
+console.log(count);
+
